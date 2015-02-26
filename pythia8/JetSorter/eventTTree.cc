22f364bf697c4bce0aa1e6751f015940935b54e6
Aman Abhishek <aman.abhishek.01@gmail.com>
2015-02-26 22:17:18 +0200

    Initial commit. Skipping all the pair production cases.

--- pythia8/JetSorter/eventTTree.cc
+++ pythia8/JetSorter/eventTTree.cc
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
   pythia.settings.listChanged();
 
   std::stringstream outputFilename("");
-  outputFilename << nEvent <<"eventsZjet_21.root";
+  outputFilename << nEvent <<"events_gammaJet.root";
 
   //ROOT TTree setup  
   TFile outFile(outputFilename.str().c_str(), "RECREATE"); //output file. change the name in physicsDef.cc too
@@ -88,20 +88,70 @@ int main(int argc, char* argv[])
   // tree->Branch("rad", &rad, "rad/F");
   // TH1D * fsr = new TH1D("fsr","fsr",300,0,1.5);
  
-  int state, count, temp,  index, identity;
+  int state, count, temp,  index, identity, simple, complex;
   // Float_t num, den;
-  vector<int> leptonListFinal;
+  int gammaIndex = -1;
+  bool skip;
 
   /****************************************END OF SET-UP**************************************************/
   for (int iEvent = 0; iEvent != nEvent; ++iEvent) 
   {
     // makeTree(event);
+    skip = false;
     if (!pythia.next()) continue;
     // cout<<"----------\n";
 
-    leptonListFinal.resize(0);
+    //leptonListFinal.resize(0);
     weight = info.weight();
     count = -1;
+
+    for(int t = 0; t != event.size(); ++t) //find gamma from the hard process
+    {
+      if(event[t].status() == -23 && event[t].id() == 22)
+      {
+        gammaIndex = t;
+        break;
+      }
+    }
+
+    assert(gammaIndex != -1);
+
+    while(!event[gammaIndex].isFinal())
+    {
+      if(event[gammaIndex].daughter1() != event[gammaIndex].daughter2()) 
+      {
+        if(abs(event[event[gammaIndex].daughter1()].id()) != 11)
+        {
+          ++complex;
+          skip = true;
+          //cout<<"--------------------------------\n";
+          //cout << "Exception: "<<gammaIndex<<endl;
+          //cout<<"--------------------------------\n";
+        }
+        
+        if(abs(event[event[gammaIndex].daughter1()].id()) == 11) 
+        {
+          ++simple;
+          skip = true;
+        }
+        break;
+      }
+      
+      else gammaIndex = event[gammaIndex].daughter1();
+      //cout<<gammaIndex<<endl;
+    }
+
+    if(skip) continue;
+
+    ++count;
+    status[count] = 2;
+    id[count] = event[gammaIndex].id();
+    pT[count] = event[gammaIndex].pT();
+    eta[count] = event[gammaIndex].eta();
+    phi[count] = event[gammaIndex].phi();
+    m[count] = event[gammaIndex].m();
+
+
 
     for(int t=0; t != event.size(); ++t)
     {
@@ -111,19 +161,14 @@ int main(int argc, char* argv[])
 
       if(event[t].isFinal())
       {
-        if(identity == 13) 
-        {
-          leptonListFinal.push_back(t);
-          continue;
-        }
-        else
+        if(t != gammaIndex)
         {
           ++count;
           status[count] = 1;
         }
       }
 
-      else if(state == 23 && identity != 13)
+      else if(state == 23 && identity != 22)
       {
         ++count;
         status[count] = 3;
@@ -139,60 +184,6 @@ int main(int argc, char* argv[])
       m[count] = event[t].m();
     }
 
-    temp = 0;
-
-    for(unsigned int x = 0; x != leptonListFinal.size(); ++x)
-    {
-      index = leptonListFinal[x];
-      while(abs(event[index].id())==13)
-      {
-        index = event[index].mother1();
-      }
-      
-      if(event[index].id()==23)
-      {
-        
-        ++count;
-        status[count] = 2;
-        ++temp;
-        if(temp==1) index = event[index].daughter1();//, cout<<index<<" ";
-        if(temp==2) index = event[index].daughter2();//, cout<<index<<" ";
-        // assert(temp<2);
-        // cout<<event[index].phi()<<" "<<event[index].eta()<<endl;
-        // if(temp == 1) daughter1 = event[index].daughter1(), a = leptonListFinal[x];
-        // if(temp == 2) daughter2 = event[index].daughter2(), b = leptonListFinal[x];
-
-        // while(abs(event[index].id())==23)
-        // {
-          // index = event[index].mother1();
-          // cout<<index<<" ";
-      }
-      
-      else
-      {
-        ++count;
-        status[count] = 1;
-      }
-      // cout<<endl;
-      
-
-      id[count] = event[index].id();
-      pT[count] = event[index].pT();
-      eta[count] = event[index].eta();
-      phi[count] = event[index].phi();
-      m[count] = event[index].m();
-      // else cout<<"Dead end.";
-      // cout<<endl;
-    }
-    // cout<<endl;
-    assert(temp==2);
-
-    // den = (event[daughter1].p()+event[daughter2].p()).pT();
-    // num = (event[a].p()+event[b].p()).pT();
-    
-    // cout<<num<<" "<<den<<endl;
-    // if(weight>1) cout<<weight<<" ";
-    // weight = num/den;
     n = UShort_t(count+1);
     tree->Fill();
     // fsr->Fill(num/den);
@@ -201,6 +192,7 @@ int main(int argc, char* argv[])
   // fsr->Write();
 
   // tree->Print();
+  cout<<simple<<" "<<complex<<endl;
   type->Write();
   tree->AutoSave("Overwrite");
   outFile.Close();
