d0e20abb0ea2dcc1d97fe161ce6bde47ec7a86bd
Aman Abhishek <aman.abhishek.01@gmail.com>
2015-03-25 13:53:33 +0200

    Major modifications: works on .root output from level 1, stylistic changes.

--- pythia8/JetSorter/plot_Zjet_channels.cc
+++ pythia8/JetSorter/plot_Zjet_channels.cc
@@ -3,39 +3,24 @@
 #include "THStack.h"
 #include <iostream>
 #include "TTree.h"
+#include "TLorentzVector.h"
 // #include "TH1.h"
 
 
 using namespace std;
 
-void ZjetPlot()
+void plot_Zjet_channels()
 {
-	int ptBins = 48.;
+	unsigned int size = 2000;
+	int ptBins = 48+6;
 	const double ptRange[]=
-    {18, 21, 24, 28, 32, 37, 43, 49, 56, 64, 74, 84,
+    {0,5,8,10,12,15,18, 21, 24, 28, 32, 37, 43, 49, 56, 64, 74, 84,
     97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468,
     507, 548, 592, 638, 686, 737, 790, 846, 905, 967,
     1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1684, 1784, 1890, 2000};
 
 	TDirectory *curdir = gDirectory;
-	TFile *f21 = new TFile("jets_output21.root","READ");
-	assert(f21 && !f21->IsZombie());
-	TTree *tree21 = (TTree*)f21->Get("tree");
-	unsigned int N21 = (unsigned int)tree21->GetEntries(); 
-	cout<<N21<<" events of 2->1 process."<<endl;
-	float Z_pt_21;
-	tree21->SetBranchAddress("Z_pT",&Z_pt_21);
-
-
-	TFile *f22 = new TFile("jets_output22.root","READ");
-	assert(f22 && !f22->IsZombie());
-	TTree *tree22 = (TTree*)f22->Get("tree");
-	unsigned int N22 = (unsigned int)tree22->GetEntries(); 
-	cout<<N22<<" events of 2->2 process."<<endl;
-	float Z_pt_22, weight;
-	tree22->SetBranchAddress("Z_pT",&Z_pt_22);
-	tree22->SetBranchAddress("weight",&weight);
-	
+		
 	TH1D* Z21 = new TH1D("21sample","21sample",ptBins,ptRange);
 	Z21->Sumw2();
 	TH1D* Z22 = new TH1D("22sample","22sample",ptBins,ptRange);
@@ -43,19 +28,74 @@ void ZjetPlot()
 	TH1D* ratio = new TH1D("ratio","ratio",ptBins,ptRange);
 	ratio->Sumw2();
 
+	TFile *f21 = new TFile("40001events_Zjet.root","READ");
+	assert(f21 && !f21->IsZombie());
+	TTree *tree21 = (TTree*)f21->Get("Events");
+	unsigned int N21 = (unsigned int)tree21->GetEntries(); 
+	cout<<N21<<" events of 2->1 process."<<endl;
+
+  	unsigned short int eventParticleCount;
+  	int id[size];
+  	float weight, pT[size], eta[size], phi[size], m[size];
+  	unsigned char status[size];
+  
+  	tree21->SetBranchAddress("n", &eventParticleCount);
+  	tree21->SetBranchAddress("weight", &weight);
+  	tree21->SetBranchAddress("id", id);
+  	tree21->SetBranchAddress("status", status);
+  	tree21->SetBranchAddress("pT", pT);
+  	tree21->SetBranchAddress("eta", eta);
+  	tree21->SetBranchAddress("phi", phi);
+  	tree21->SetBranchAddress("m", m);
+
+	TLorentzVector v1;    
 	for(unsigned int x=0; x != N21; ++x)
 	{
 		tree21->GetEntry(x);
-		Z21->Fill(Z_pt_21);
+		TLorentzVector v;
+		for(unsigned int y = 0; y< eventParticleCount; ++y)
+		{
+			if(status[y]==2)
+			{
+				v1.SetPtEtaPhiM(pT[y],eta[y],phi[y],m[y]);
+				v += v1;
+			}
+		}
+		Z21->Fill(v.Pt(),weight);
+		//cout<<v.Pt()*weight<<endl;
 	}
 
+	TFile *f22 = new TFile("40000events_Zjet.root","READ");
+	assert(f22 && !f22->IsZombie());
+	TTree *tree22 = (TTree*)f22->Get("Events");
+	unsigned int N22 = (unsigned int)tree22->GetEntries(); 
+	cout<<N22<<" events of 2->1 process."<<endl;
+  
+  	tree22->SetBranchAddress("n", &eventParticleCount);
+  	tree22->SetBranchAddress("weight", &weight);
+  	tree22->SetBranchAddress("id", id);
+  	tree22->SetBranchAddress("status", status);
+  	tree22->SetBranchAddress("pT", pT);
+  	tree22->SetBranchAddress("eta", eta);
+  	tree22->SetBranchAddress("phi", phi);
+  	tree22->SetBranchAddress("m", m);
 
+ 
 	for(unsigned int x=0; x != N22; ++x)
 	{
 		tree22->GetEntry(x);
-		Z22->Fill(Z_pt_22,weight);
+		TLorentzVector v2;
+		for(unsigned int y = 0; y< eventParticleCount; ++y)
+		{
+			if(status[y]==2)
+			{
+				v1.SetPtEtaPhiM(pT[y],eta[y],phi[y],m[y]);
+				v2 += v1;
+			}
+		}
+		Z22->Fill(v2.Pt(),weight);
+		
 	}
-	ratio->Divide(Z22,Z21,1,0.001);
 
 
 	// TCanvas *c1 = new TCanvas("c1", "c1",900, 700);
@@ -67,24 +107,19 @@ void ZjetPlot()
 
 	// Z21->Draw("SAMEE");
 	// Z21->SetLineColor(kRed);
+	ratio->Divide(Z22,Z21,1,0.001);
 	Z21->Scale(0.0009);
+	//Z22->Draw();
 	
-	// c1->cd(2);
-	// gPad->SetLogy();
-	
-	// ratio->Draw();
-	// ratio->GetXaxis()->SetRange(0,300);
-	// ratio->SetLineColor(kGreen+3);
-
-	// 
 	setTDRStyle();
 	TH1D *h1 = new TH1D("h1",";p_{T} (GeV);Cross section",100,0,1000);
-	h1->SetMinimum(2*1e-4);
-	h1->SetMaximum(2*1e2);
-	h1->GetYaxis()->SetNoExponent();
-	h1->GetXaxis()->SetNoExponent();
-	h1->GetXaxis()->SetMoreLogLabels(kTRUE);
-	h1->GetXaxis()->SetRangeUser(20,1000);
+	h1->SetMinimum(0.5*1e-7);
+	h1->SetMaximum(1*1e1);
+	//h1->GetYaxis()->SetNoExponent();
+	//h1->GetXaxis()->SetNoExponent();
+	//h1->GetXaxis()->SetMoreLogLabels(kTRUE);
+	h1->GetXaxis()->SetRangeUser(10,1000);
+	//h1->GetXaxis()->LabelsOption("v");
 	// h1->GetYaxis()->SetMoreLogLabels(kTRUE);
 	TH1D *h2 = new TH1D("h2",";p_{T} (GeV);(241+242)/221",100,0,1000);
 	h2->SetMaximum(1.6);
@@ -92,15 +127,15 @@ void ZjetPlot()
 	h2->GetXaxis()->SetNoExponent();
 	h2->GetYaxis()->SetNoExponent();
 	h2->GetXaxis()->SetMoreLogLabels(kTRUE);
-	h2->GetXaxis()->SetRangeUser(20,1000);
+	h2->GetXaxis()->SetRangeUser(10,1000);
 	// h2->GetYaxis()->SetMoreLogLabels(kTRUE);
 	TCanvas *c2 = tdrDiCanvas("c2",h1,h2,0,33);
 	c2->cd(1);
 	gPad->SetLogy();
 	gPad->SetLogx();
 	// gStyle->SetOptStat(kFALSE);
-	tdrDraw(Z22,"HISTE",kDot,kBlue,kSolid,-1,1001,kBlue-6);
-	tdrDraw(Z21,"PE",kMultiply, kBlue+4);
+	tdrDraw(Z22,"PE",kFullCircle,kBlue);
+	tdrDraw(Z21,"PE",kFullTriangleUp, kRed-6);
 	
 	c2->cd(2);
 	gPad->SetLogx();
